fb3e1a6b49a630fb07e500a9d85b33a52d25e2e7
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-03-04 16:11:27 +0100

    Add: model

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller {
     function index () {
-    return view('home');
+    $data = Movie::all();
+
+    dump($data);
+
+    return view('home', compact('data'));
     }
 };
