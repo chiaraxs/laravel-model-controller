ebcea29af7eb110394756fd43c7c510fb2f85e60
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-03-04 16:25:03 +0100

    Add bootstrap

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,6 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Home</title>
+    
+    {{-- richiamo al mio app.css --}}
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    {{-- /richiamo al mio app.css --}}
+
+
 </head>
 <body>
 
